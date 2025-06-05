5dee04cbc7aed17c8ce79a3696f3089b5825a7c8
morronepablo <morronepablo@gmail.com>
2025-06-05 16:26:06 -0300

    agregando estilos

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,6 +6,7 @@ const Sidebar = () => {
         <div className="top">
             <span className="logo">lamadmin</span>
         </div>
+        <hr />
         <div className="center">
             <ul>
                 <li>
